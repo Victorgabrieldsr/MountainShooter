b8856e00a2abeec74336525d8ea19c69d8326139
Victorgabrieldsr <victorgabrieldsr@gmail.com>
2025-08-17 15:14:10 -0300

    adding classes generated from StarUML

---
+++ code/lavel.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Lavel:
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
